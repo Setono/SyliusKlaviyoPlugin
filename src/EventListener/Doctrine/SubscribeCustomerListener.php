46ffe0612a48610883f6bf81f3cd6d06e4e00e82
Joachim Løvgaard <joachim@loevgaard.dk>
2021-10-06 15:47:14 +0200

    Subscribe customer

---
+++ src/EventListener/Doctrine/SubscribeCustomerListener.php
@@ -0,0 +1,81 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Setono\SyliusKlaviyoPlugin\EventListener\Doctrine;
+
+use Doctrine\ORM\Event\LifecycleEventArgs;
+use Doctrine\ORM\Event\PreUpdateEventArgs;
+use Setono\SyliusKlaviyoPlugin\Message\Command\SubscribeCustomer;
+use Setono\SyliusKlaviyoPlugin\Model\MemberListInterface;
+use Setono\SyliusKlaviyoPlugin\Repository\MemberListRepositoryInterface;
+use Sylius\Component\Channel\Context\ChannelContextInterface;
+use Sylius\Component\Customer\Model\CustomerInterface;
+use Symfony\Component\Messenger\MessageBusInterface;
+
+final class SubscribeCustomerListener
+{
+    private MessageBusInterface $commandBus;
+
+    private ChannelContextInterface $channelContext;
+
+    private MemberListRepositoryInterface $memberListRepository;
+
+    public function __construct(
+        MessageBusInterface $commandBus,
+        ChannelContextInterface $channelContext,
+        MemberListRepositoryInterface $memberListRepository
+    ) {
+        $this->commandBus = $commandBus;
+        $this->channelContext = $channelContext;
+        $this->memberListRepository = $memberListRepository;
+    }
+
+    public function postPersist(LifecycleEventArgs $eventArgs): void
+    {
+        $customer = $eventArgs->getEntity();
+        if (!$customer instanceof CustomerInterface) {
+            return;
+        }
+
+        if (!$customer->isSubscribedToNewsletter()) {
+            return;
+        }
+
+        $this->subscribe($customer);
+    }
+
+    public function preUpdate(PreUpdateEventArgs $eventArgs): void
+    {
+        $customer = $eventArgs->getEntity();
+        if (!$customer instanceof CustomerInterface) {
+            return;
+        }
+
+        if (!$eventArgs->hasChangedField('subscribedToNewsletter') || $eventArgs->getNewValue('subscribedToNewsletter') === false) {
+            return;
+        }
+
+        $this->subscribe($customer);
+    }
+
+    private function subscribe(CustomerInterface $customer): void
+    {
+        foreach ($this->getListIds() as $listId) {
+            $this->commandBus->dispatch(new SubscribeCustomer($customer, $listId));
+        }
+    }
+
+    /**
+     * @return array<array-key, string>
+     */
+    private function getListIds(): array
+    {
+        return array_map(
+            static function (MemberListInterface $list): string {
+                return (string) $list->getKlaviyoId();
+            },
+            $this->memberListRepository->findByChannel($this->channelContext->getChannel())
+        );
+    }
+}
---
+++ src/Message/Command/SubscribeCustomer.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Setono\SyliusKlaviyoPlugin\Message\Command;
+
+use Sylius\Component\Customer\Model\CustomerInterface;
+use Webmozart\Assert\Assert;
+
+final class SubscribeCustomer
+{
+    private int $customerId;
+
+    private string $listId;
+
+    /**
+     * @param int|CustomerInterface|mixed $customer
+     */
+    public function __construct($customer, string $listId)
+    {
+        if ($customer instanceof CustomerInterface) {
+            $customer = (int) $customer->getId();
+        }
+        Assert::integer($customer);
+
+        $this->customerId = $customer;
+        $this->listId = $listId;
+    }
+
+    public function getCustomerId(): int
+    {
+        return $this->customerId;
+    }
+
+    public function getListId(): string
+    {
+        return $this->listId;
+    }
+}

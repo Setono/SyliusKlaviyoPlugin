1dff256007289db4c4530256231f6e9ee0dbc4e7
Joppe De Cuyper <hello@joppe.dev>
2024-01-25 12:10:15 +0100

    Remove monolog

--- tests/Application/config/bundles.php
+++ tests/Application/config/bundles.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
-    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],

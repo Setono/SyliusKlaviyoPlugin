1b23b44d1acf33a08fad175aa2005254d683af7b
Joppe De Cuyper <hello@joppe.dev>
2024-01-28 22:36:40 +0100

    Monolog config

--- tests/Application/config/bundles.php
+++ tests/Application/config/bundles.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
